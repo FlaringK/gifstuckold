75dfea18909122fb24dc7a868093df86a3ee3c77
FlaringK <caelans@iinet.net.au>
2022-01-12 19:47:01 +0800

    Done!

--- Alchemy/alchemy.js
+++ Alchemy/alchemy.js
@@ -23,6 +23,8 @@ let addtypes = () => {
   }
 }
 
+addtypes()
+
 //Make image
 
 var canvas = document.getElementById('bitmap');
@@ -32,6 +34,7 @@ let createAlchemy = () => {
   var assets = document.querySelectorAll("#preload img")
   var options = document.querySelectorAll("#useroptions select")
   var uploads = Array.prototype.slice.call(document.querySelectorAll("#uploadbg img"))
+  var name = document.getElementById("name").value.toUpperCase()
 
   var bg = assets[3]
   var box = assets[4]
@@ -61,6 +64,38 @@ let createAlchemy = () => {
     context.drawImage(uploads[i], e + 5, 97, 80, 80)
   })
 
-  // Write cost and text
+  // Write Name
+  context.font = "16.7pt Verdana"
+  context.fillStyle='#C6C7C7';
+  context.textAlign='center';
+  context.textBaseline='alphabetic';
   
+  context.fillText(name, 325, 236);
+  
+  // Write Cost
+  var imgs = document.querySelectorAll("#gristimg img")
+  var types = document.querySelectorAll("#gristwrap #grist")
+  var costs = document.querySelectorAll("#gristwrap #cost")
+  var gristLengths = []
+
+  context.font = "16.7pt Verdana"
+  context.textAlign='left';
+  context.fillStyle='#21AFEE';
+
+  //context.fillText("000", 325, 267);
+  var totalLength = 0
+  costs.forEach(e => {
+    var length = e.value.length * 14 + 36
+    gristLengths.push(length)
+    totalLength += length
+  })
+  
+  var startpos = 325 - (totalLength / 2)
+  var currentpos = startpos
+  gristLengths.forEach((e, i) => {
+    //context.fillRect(currentpos, 250, e, 16);
+    context.fillText(costs[i].value, currentpos + 32, 267)
+    context.drawImage(imgs[types[i].value], currentpos + 3, 248, 24, 24);
+    currentpos += e + 1
+  })
 }
