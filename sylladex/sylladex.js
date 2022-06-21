00cd850d5eb067d0a68c881a8525b33cfd3f3eb7
FlaringK <caelans@iinet.net.au>
2022-06-21 17:14:02 +0800

    GO!

--- sylladex/sylladex.js
+++ sylladex/sylladex.js
@@ -2,11 +2,11 @@
 // init canvas //
 /////////////////
 
-var canvas = document.getElementById('bitmap');
-var context = canvas.getContext('2d');
+let canvas = document.getElementById('bitmap');
+let context = canvas.getContext('2d');
 
 let clearcanvas = () => { 
-  var fill = context.fillStyle; // save current fillstyle
+  let fill = context.fillStyle; // save current fillstyle
   context.fillStyle = 'rgb(255,255,255)';
   context.fillRect(0,0,canvas.width, canvas.height); 
   context.fillStyle = fill; // reset to saved fillstyle
@@ -18,8 +18,8 @@ clearcanvas() // GIF can't do transparent so do white
 //////////////////
 
 let previewUpload = (event, element, key) => {
-  var output = element.nextElementSibling;
-  var source = URL.createObjectURL(event.target.files[0])
+  let output = element.nextElementSibling;
+  let source = URL.createObjectURL(event.target.files[0])
   output.src = source;
   output.onload = function() {
     URL.revokeObjectURL(output.src) // free memory
@@ -31,32 +31,32 @@ let updatecardlist = cardnumber => {
   cardlist.innerHTML = ""
 
   for (let index = 0; index < cardnumber; index++) {
-    var id = "card" + index
+    let id = "card" + index
 
     //create upload box
-    var div = document.createElement('div')
+    let div = document.createElement('div')
     div.id = "carddiv" + index
 
-    var label = document.createElement('label')
+    let label = document.createElement('label')
     label.for = id
     label.innerText = "Select image for card " + (index+1) + ":"
 
-    var input = document.createElement('input')
+    let input = document.createElement('input')
     input.type = "file"
     input.id = id
     input.name = id
     input.accept = "image/*"
     input.setAttribute("onchange", "previewUpload(event, this, " + (index + 2) + ")")
 
-    var img = document.createElement('img')
+    let img = document.createElement('img')
     img.src = "assets/placeholders/temp item.png"
     img.setAttribute("class", "uploadedItem")
 
-    var namelabel = document.createElement('label')
+    let namelabel = document.createElement('label')
     namelabel.for = id + "name"
     namelabel.innerText = "Item name:"
 
-    var name = document.createElement('input')
+    let name = document.createElement('input')
     name.id = id + "name"
     name.name = id + "name"
     name.value = "Perfectly generic cubes"
@@ -79,7 +79,7 @@ let selectModus = value => {
   button.setAttribute("onclick", "createGif" + value + "()")
 
   let createblankinput = () => {
-    var blankcards = document.createElement('input')
+    let blankcards = document.createElement('input')
     blankcards.type = "number";
     blankcards.id = "blankcards"
     blankcards.value = 2
@@ -87,15 +87,15 @@ let selectModus = value => {
   }
 
   let createchoosecardinput = () => {
-    var choosecard = document.createElement('input')
+    let choosecard = document.createElement('input')
     choosecard.type = "number";
     choosecard.id = "choosecard"
     choosecard.value = 1
     optionslist.push([choosecard, "If applicable, choose which card to eject: "]);
   }
 
-  var assetcount = 0
-  var optionslist = []
+  let assetcount = 0
+  let optionslist = []
   switch (value) {
     case "hashmap":
       assetcount = 12
@@ -109,6 +109,16 @@ let selectModus = value => {
 
     case "filo":
       assetcount = 3
+      createblankinput()
+      break;
+    
+    case "wallet":
+      assetcount = 4
+      createblankinput()
+      break;
+
+    case "walletNoBar":
+      assetcount = 4
       createblankinput()
       break;
 
@@ -125,7 +135,7 @@ let selectModus = value => {
   //load assets and values
   loadGifassets(value, assetcount)
   optionslist.forEach(e => {
-    var label = document.createElement('label')
+    let label = document.createElement('label')
     label.innerText = e[1]
     options.appendChild(label)
     options.appendChild(e[0])
@@ -137,7 +147,7 @@ let selectModus = value => {
 // Load Assets //
 /////////////////
 
-var loadtally = 0
+let loadtally = 0
 let tallyLoads = max => {
   loadtally++
   document.getElementById('loadtally').innerText = loadtally + " / " + max
@@ -150,8 +160,8 @@ let loadGifassets = (file, count) => {
   document.getElementById('loadtally').innerText = "0 / 0"
 
   for (let index = 0; index < count; index++) {
-    var path = "assets/" + file + "/" + index + ".png";
-    var img = document.createElement('img');
+    let path = "assets/" + file + "/" + index + ".png";
+    let img = document.createElement('img');
     img.src = path;
     document.getElementById('preload').appendChild(img);
     img.onload = tallyLoads(count)
@@ -174,15 +184,15 @@ class basicModus {
   //Draw Card Bar
   drawcardbar = Vpos => {
     this.barheight = 100
-    var topbar = canvas.height - this.barheight + Vpos
+    let topbar = canvas.height - this.barheight + Vpos
     context.drawImage(this.cardbar, 0, topbar)
   
-    var itemcount = this.displaylist.length
+    let itemcount = this.displaylist.length
   
     this.cardwidth = 56
-    var cardcount = parseInt(itemcount) + parseInt(this.blankcount)
-    var cardlistwidth = cardcount * this.cardwidth
-    var cardlistStartpos = (canvas.width / 2) - (cardlistwidth / 2)
+    let cardcount = parseInt(itemcount) + parseInt(this.blankcount)
+    let cardlistwidth = cardcount * this.cardwidth
+    let cardlistStartpos = (canvas.width / 2) - (cardlistwidth / 2)
   
     this.drawcardbarcards(cardcount, cardlistStartpos, topbar)
   }
@@ -190,8 +200,8 @@ class basicModus {
   //Draw bar card deck
   drawcardbarcards = (cardcount, startpos, topbar) => {
     for (let i = 0; i < cardcount; i++) {
-      var xpos =  startpos + i * this.cardwidth
-      var ypos =  topbar + 30
+      let xpos =  startpos + i * this.cardwidth
+      let ypos =  topbar + 30
       context.drawImage(this.cardmini, xpos, ypos)
   
       if (this.displaylist[i]) {
@@ -202,8 +212,8 @@ class basicModus {
   
   // Draw one card at index
   drawcard = (index, itemimg, bumnp) => {
-    var xpos = 12 + 23 * index
-    var ypos = 10 + 23 * index
+    let xpos = 12 + 23 * index
+    let ypos = 10 + 23 * index
     if (bumnp) {ypos += 2; xpos += 2}
   
     context.drawImage(this.cardback, xpos, ypos)
@@ -213,17 +223,26 @@ class basicModus {
   // Draw deck of cards
   drawcarddeck = () => {
     this.displaylist.forEach((e, index) => {
-      var i = this.displaylist.length - index - 1
+      let i = this.displaylist.length - index - 1
       this.drawcard(i, this.displaylist[i])
     });
   }
   
   // Render intro & outro
   renderintro = ifBackwards => {
+
+    // Draw inital frame without bar
+    if (!ifBackwards) {
+      clearcanvas();
+      context.drawImage(this.bg, 0, 0)
+      this.drawcarddeck()
+      this.gif.addFrame(canvas, {copy: true, delay: 500});
+    }
+
     // Draw n frames of bar fade in
-    var n = 5
+    let n = 5
     for (let i = 0; i < n; i++) {
-      var j = ifBackwards ? n - i - 1 : i
+      let j = ifBackwards ? n - i - 1 : i
   
       clearcanvas();
       context.drawImage(this.bg, 0, 0)
@@ -232,6 +251,14 @@ class basicModus {
       this.drawcardbar(100 - (50 + j * (50 / (n-1))))
       context.globalAlpha = 1
       this.gif.addFrame(canvas, {copy: true, delay: 300/n});
+    }
+
+    if (ifBackwards) {
+      //Draw final frame
+      clearcanvas();
+      context.drawImage(this.bg, 0, 0)
+      this.drawcarddeck()
+      this.gif.addFrame(canvas, {copy: true, delay: 500})
     }
   }
   
@@ -249,7 +276,7 @@ class basicModus {
   enddeck = pick => pick ? Array.from(this.uploadlist).splice(1) : Array.from(this.uploadlist)
 
   getassetlist = () => {
-    var list = document.querySelectorAll("#preload img")
+    let list = document.querySelectorAll("#preload img")
     this.cardback = list[0]
     this.cardbar = list[1]
     this.cardmini = list[2]
@@ -262,7 +289,7 @@ class basicModus {
   
       //draw card deck
       this.displaylist.forEach((e, index) => {
-        var i = this.displaylist.length - index - 1
+        let i = this.displaylist.length - index - 1
         if (i == j) {
           this.drawcard(i, this.displaylist[i], true)
         } else if ((i !== this.displaylist.length - 1 && !pick) || pick) {
@@ -279,6 +306,16 @@ class basicModus {
       }
     }
   }
+
+  extraAnimationBefore = pick => {
+    this.drawhold()
+    this.gif.addFrame(canvas, {copy: true, delay: 500});
+  }
+
+  extraAnimationAfter = pick => {
+    this.drawhold()
+    this.gif.addFrame(canvas, {copy: true, delay: 1500});
+  }
   
   // ----------- CREATE THE GIF ----------- //
   
@@ -289,7 +326,7 @@ class basicModus {
     const sdexaction = document.getElementById('sdex').value
     const itemaction = document.getElementById('item').value
     const backgrounds = document.querySelectorAll(".uploadedbg")
-    var tickback = document.getElementById("check").checked
+    let tickback = document.getElementById("check").checked
 
     //get bg
     this.bg = backgrounds[0]
@@ -298,8 +335,9 @@ class basicModus {
     this.blankcount = this.getblanks()
   
     // init pick
-    var pick = itemaction == "pick" ? true : false
-    this.displaylist = this.startdeck( pick)
+    let pick = itemaction == "pick" ? true : false
+    this.pick = pick
+    this.displaylist = this.startdeck(pick)
     this.blankcount += pick ? 1 : 0
   
     //Drawing!!!
@@ -308,46 +346,30 @@ class basicModus {
       quality: 10
     });
   
-    //STEP ONE: if intro
-    if (sdexaction == 'open') {
-      // Draw inital frame without bar
-      clearcanvas();
-      context.drawImage(backgrounds[0], 0, 0)
-      this.drawcarddeck()
-      this.gif.addFrame(canvas, {copy: true, delay: 500});
-  
+    //STEP 1: if intro
+    if (sdexaction.includes("open")) {
       this.renderintro(false)
     }
+
+    //STEP 2: hold frame, all on screen
+    this.extraAnimationBefore(pick)
+
   
-    
-    //STEP TWO: hold frame, all on screen
-    this.drawhold()
-    this.gif.addFrame(canvas, {copy: true, delay: 500});
-  
-  
-    //STEP THREE: change bg & bumnp cards
+    //STEP 3: change bg & bumnp cards
     pick ^= true;
     this.displaylist = this.enddeck(pick)
     this.blankcount += pick ? 1 : -1
     this.bg = backgrounds[1]
   
     this.bumnpcards(pick, tickback, backgrounds[2])
-  
-  
+
     //STEP 4: hold frame 2
-    this.drawhold()
-    this.gif.addFrame(canvas, {copy: true, delay: 1500});
+    this.extraAnimationAfter(pick)
   
   
     //STEP 5: if close
-    if (sdexaction == 'close') {
+    if (sdexaction.includes("close")) {
       this.renderintro(true)
-  
-      //Draw final frame
-      clearcanvas();
-      context.drawImage(backgrounds[1], 0, 0)
-      this.drawcarddeck()
-      this.gif.addFrame(canvas, {copy: true, delay: 500})
     }
   
     // DISPLAY RESULT!!!
@@ -379,8 +401,8 @@ let filo = new basicModus
 
 //Change deck layout
 filo.drawcard = (index, itemimg, bumnp) => {
-  var xpos = 12 + 18 * index
-  var ypos = 10 + 5 * index
+  let xpos = 12 + 18 * index
+  let ypos = 10 + 5 * index
   if (bumnp) {ypos += 2; xpos += 2}
 
   context.drawImage(filo.cardback, xpos, ypos)
@@ -390,8 +412,8 @@ filo.drawcard = (index, itemimg, bumnp) => {
 //align card bar
 filo.drawcardbarcards = (cardcount, startpos, topbar) => {
   for (let i = 0; i < cardcount; i++) {
-    var xpos =  startpos + i * filo.cardwidth
-    var ypos =  topbar + 30
+    let xpos =  startpos + i * filo.cardwidth
+    let ypos =  topbar + 30
     context.drawImage(filo.cardmini, xpos, ypos)
 
     if (i > filo.blankcount - 1) {
@@ -413,8 +435,8 @@ let array = new basicModus
 
 //Change deck layout
 array.drawcard = (index, itemimg, bumnp) => {
-  var xpos = 12 + 16 * index
-  var ypos = 10 + 23 * index
+  let xpos = 12 + 16 * index
+  let ypos = 10 + 23 * index
   if (bumnp) {ypos += 2; xpos += 2}
 
   context.drawImage(array.cardback, xpos, ypos)
@@ -431,4 +453,95 @@ array.enddeck = pick => {
 
 let createGifarray = () => {
   array.createGIF()
+}
+
+// ----------- WALLET ----------- //
+// THIS IS VERY BAD DON'T FOLLOW THIS STRUCTURE
+
+let wallet = new basicModus
+
+//Change deck layout
+wallet.drawcard = (index, itemimg, bumnp) => {4
+  xpos = bumnp ? 127 : 129
+  ypos = bumnp ? 23 : 25
+
+  context.drawImage(wallet.walletback, 11, 5)
+  context.drawImage(wallet.cardback, 127, 23)
+
+  if (wallet.bg == document.querySelectorAll(".uploadedbg")[1] && wallet.pick) {
+    let lastItem = Array.prototype.slice.call(document.querySelectorAll(".uploadedItem")).pop()
+    context.drawImage(lastItem, xpos + 11, ypos + 25, 60, 75)
+  }
+}
+
+wallet.drawcarddeck = () => {
+  context.drawImage(wallet.walletback, 11, 5)
+}
+
+wallet.getassetlist = () => {
+  let list = document.querySelectorAll("#preload img")
+  wallet.cardback = list[0]
+  wallet.cardbar = list[1]
+  wallet.cardmini = list[2]
+  wallet.walletback = list[3]
+}
+
+wallet.extraAnimationBefore = (pick) => {
+  let xPosArray = [23, 54, 131, 127]
+  let frameLengths = [500, 50, 50, 1000]
+  ypos = 23
+
+  for (let i = 0; i < xPosArray.length; i++) {
+    clearcanvas();
+    context.drawImage(wallet.bg, 0, 0)
+    wallet.drawcardbar(0)
+
+    context.drawImage(wallet.cardback, xPosArray[i], ypos)
+    if (!pick) {
+      let itemimg = Array.prototype.slice.call(document.querySelectorAll(".uploadedItem")).pop()
+      context.drawImage(itemimg, xPosArray[i] + 11, ypos + 25, 60, 75)
+    }
+    context.drawImage(wallet.walletback, 11, 5)
+
+    wallet.gif.addFrame(canvas, {copy: true, delay: frameLengths[i]})
+  }
+}
+
+wallet.extraAnimationAfter = (pick) => {
+  let xPosArray = [23, 54, 131, 127].reverse()
+  let frameLengths = [1000, 50, 50, 1000]
+  ypos = 23
+
+  for (let i = 0; i < xPosArray.length; i++) {
+    clearcanvas();
+    context.drawImage(wallet.bg, 0, 0)
+    wallet.drawcardbar(0)
+
+    context.drawImage(wallet.cardback, xPosArray[i], ypos)
+    if (!pick) {
+      let itemimg = Array.prototype.slice.call(document.querySelectorAll(".uploadedItem")).pop()
+      context.drawImage(itemimg, xPosArray[i] + 11, ypos + 25, 60, 75)
+    }
+    context.drawImage(wallet.walletback, 11, 5)
+
+    wallet.gif.addFrame(canvas, {copy: true, delay: frameLengths[i]})
+  }
+}
+
+// THIS IS REALLY BAD PRACTICE HOLY SHIT
+// DON'T DO THIS AGAIN
+
+const walletRenderIntro = wallet.renderintro.bind({})
+const walletDrawCardBar = wallet.drawcardbar.bind({})
+
+let createGifwallet = () => {
+  wallet.renderintro = walletRenderIntro
+  wallet.drawcardbar = walletDrawCardBar
+  wallet.createGIF()
+}
+
+let createGifwalletNoBar = () => {
+  wallet.renderintro = lmao => {}
+  wallet.drawcardbar = hehe => {}
+  wallet.createGIF()
 }
